856a4656a8464e53ad598a3221fee5e622278682
Jorge Montalban <jmontalbanbenites@gmail.com>
2026-05-14 17:47:35 -0500

    agregue un ejemplo

--- dia_2/1_colecciones_de_datos.py
+++ dia_2/1_colecciones_de_datos.py
@@ -1,28 +1,4 @@
-#arrays
 
-frutas = ['Manzana','Platano','Papaya', 'pitahaya']
-print(frutas[1])
-frutas.append('Mandarina')
-
-persona = {
-    'nombre' : 'eduardo',
-    'apellido' : 'de rivero',
-    'correo' : 'ederrivero@gmail.com',
-    'hobbies' : ['comer','programar','montar vici'],
-    'direcciones': {
-        'calle': 'calle los geraneos',
-        'numero': 870,
-        'postal': '04010'
-
-    },
-    'viudo': False,
-    'familiares': ('Juanito Perez','Maria Aguilar','Roxana Washington')
-}
-
-print(persona['nombre'])
-print(persona['hobbies'])
-print(len(persona['hobbies']))
-print(persona['direcciones']['calle'])
 
 curso = {
     'nombre': 'Backend',
@@ -48,3 +24,10 @@ curso = {
     'costo': 550.76,
     'descanso': 'a veces'
 }
+
+print(curso['nombre'],curso['duracion'])
+print(len(curso['topics']))
+print(curso['topics'][0],curso['topics'][1])
+print(len(curso['semanas']))
+print(curso['semanas'][0]['descripcion'])
+print(curso['habilidades'][1])
