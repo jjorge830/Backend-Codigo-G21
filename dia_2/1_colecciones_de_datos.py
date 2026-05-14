ba49e8889c75875c87f1c5df7e72d9e26171fea8
Jorge Montalban <jmontalbanbenites@gmail.com>
2026-05-14 17:15:54 -0500

    uso de colecciones

--- dia_2/1_colecciones_de_datos.py
+++ dia_2/1_colecciones_de_datos.py
@@ -23,3 +23,28 @@ print(persona['nombre'])
 print(persona['hobbies'])
 print(len(persona['hobbies']))
 print(persona['direcciones']['calle'])
+
+curso = {
+    'nombre': 'Backend',
+    'duracion': '10 semanas',
+    'fecha_inicio': '2024-11-11',
+    'fecha_fin': '2025-01-30',
+    'topics': ['Python', 'Express', 'Django', 'Flask'],
+    'semanas': [
+        {
+            'nombre': 'semana 01',
+            'descripcion': 'intro a python'
+        },
+        {
+            'nombre': 'semana 02',
+            'descripcion': 'base de datos'
+        }
+    ],
+    'habilidades': ('logica de programacion', 'manejo de eventos', 'despliegue en servidores'),
+    'finalizado': False,
+    'profesores': {
+        'Arnold Gallegos', 'Eduardo de Rivero'
+    },
+    'costo': 550.76,
+    'descanso': 'a veces'
+}
